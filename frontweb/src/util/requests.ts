1d3ad9df9166d1a2c3b1daa17c4073b7b8be9a06
Alex Euzebio <alex.euzebio@gmail.com>
2022-03-15 16:58:07 -0300

    Authorized request

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,12 +1,24 @@
 //Rotinas para requisições
 
 import qs from 'qs';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+type loginResponse = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  userFirstName: string;
+  userId: number;
+};
+
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
+
+const tokenKey = 'authData';
 
 /******* Requisição de login
 
@@ -26,25 +38,52 @@ Mas eles tem que ser convertidos em form-urlencoded com o qs
 type LoginData = {
   username: string;
   password: string;
-}
+};
 
 export const requestBackendLogin = (loginData: LoginData) => {
-
   const headers = {
-    Authorization: 'Basic ' + window.btoa( CLIENT_ID + ':' + CLIENT_SECRET),
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
+    Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
 
   const data = qs.stringify({
     ...loginData,
-    grant_type: 'password'
-  })
+    grant_type: 'password',
+  });
 
   return axios({
     method: 'POST',
     baseURL: BASE_URL,
     url: '/oauth/token',
     data: data,
-    headers: headers
-  })
-}
+    headers: headers,
+  });
+};
+
+export const requestBackend = (config: AxiosRequestConfig) => {
+  const headers = config.withCredentials
+    ? {
+        ...config.headers,
+        Authorization: 'Bearer ' + getAuthData().access_token,
+      }
+    : config.headers;
+  return axios({ ...config, baseURL: BASE_URL, headers });
+};
+
+/******** Salvando os dados de autenticação no Local Storage
+ *
+ */
+
+export const saveAuthData = (obj: loginResponse) => {
+  localStorage.setItem(tokenKey, JSON.stringify(obj));
+};
+
+/******** Obtendo os dados da autenticação
+ *
+ */
+
+export const getAuthData = () => {
+  const str = localStorage.getItem(tokenKey) ?? '{}';
+  const obj = JSON.parse(str) as loginResponse;
+  return obj;
+};
